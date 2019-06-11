7388e39ef69e26bad1141d1a478a50463e57e825
Marcos Navarro <marcos@aftertype.com>
2019-06-11 11:33:37 -0600

    Added CircleCI

--- src/constants/theme.ts
+++ src/constants/theme.ts
@@ -1,13 +1,13 @@
 export const theme: { [key: string]: string } = {
-  WHITE: '#FFF',
   BLACK: '#000',
-  DARK: '#303030',
   BLUE: '#009CE5',
-  RED: '#FF2121',
   BRIGHT_GREEN: '#00E556',
+  DARK: '#303030',
   GD_BLUE: '#238BFF',
-  GD_PURPLE: '#7D05AA',
   GD_DARK_PURPLE: '#47004B',
   GD_DARK_RED: '#5A0606',
   GD_LIGHT_RED: '#FF2323',
-}
+  GD_PURPLE: '#7D05AA',
+  RED: '#FF2121',
+  WHITE: '#FFF',
+};
